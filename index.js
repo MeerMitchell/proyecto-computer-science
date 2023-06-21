8ad896b47ae9b01679116c48b4e533a37006761c
malfoy.obsession <malfoy.obsession@gmail.com>
2023-06-20 22:36:24 -0600

    feat: se anadió funcion printHogwarts y buscarElemento

--- index.js
+++ index.js
@@ -1,29 +1,53 @@
+function traerDatos(){
+    fetch('https://hp-api.onrender.com/api/characters')
+    .then(response => response.json())
+    .then(json => {
+        return json 
+   // printHogwarts(json);
 
+    });
+}
 
-fetch('https://hp-api.onrender.com/api/characters')
-        .then(response => response.json())
-        .then(json => {
-            printHogwarts(json.results);
-           
-        });
+const json = traerDatos();
+console.log(json);
+
 
 
 function printHogwarts(character) {
     const container = document.getElementById('container')
     character.forEach(character => {
-        container.innerHTML = `
-        ${container.innerHTML}
-        <div class="card">
-        <img src="https://ik.imagekit.io/hpapi/${getCharacterId(character.url)}.jpg"/>
-        <span>Nº.${getCharacterId(getCharacterId.url)}</span>
-        <h2>${character.name.charAt(0).toUpperCase() + character.name.slice(1)}</h2>
-        </card>
-        
-`;
-});
-}   
+        console.log(character);
 
-function getCharacterId(url) {
-    return url.replace('https://hp-api.onrender.com/api/characters/', '').replace('/','')
+        const nameCharacter = document.createElement("p");
+        nameCharacter.textContent = `${character.name}`;
+        container.appendChild(nameCharacter);
+
+        const photoCharacter = document.createElement("img");
+        if (character.image == ""){
+            photoCharacter.setAttribute("src", "./assets/profl.png");
+        } else {
+            photoCharacter.setAttribute("src", `${character.image}`);
+        }
+
+        container.appendChild(photoCharacter);
+
+    });
 }
+
+function buscarElemento(){
+    const input = document.getElementById('characterSearch');
+    const valor = input.value.toLowerCase();
+    const resultadosElemento = document.getElementById('resultados');
+
+    resultadosElemento.innerHTML = "";
+
+    const resultados = json.filter(item => item.name.toLowerCase().includes(valor));
+
+    resultados.forEach(item => {
+        const li = document.createElement("li");
+        li.textContent = item.name;
+        resultadosElemento.appendChild(li);
+    })
+}
+
 
